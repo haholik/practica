2cb5c26fef210f6a21a3ba98af2ef1a92423fb29
haholik <60041113+haholik@users.noreply.github.com>
2022-12-02 09:44:12 +0500

    add

---
+++ z6.py
@@ -0,0 +1,11 @@
+import re
+
+get_ip = input("Введите ip в формате xxx.xxx.xxx.xxx/port")
+lizt = re.split("[/.]", get_ip)
+
+print("Network: \n", lizt[0],lizt[1],lizt[2],lizt[3])
+print(bin(int(lizt[0])).replace("0b",""),bin(int(lizt[1])).replace("0b",""),
+      bin(int(lizt[2])).replace("0b",""),bin(int(lizt[3])).replace("0b",""))
+print("Mask: \n")
+print("1" * 28 + "0" * 4)
+
