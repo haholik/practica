fbdb26a3025ae3a3d6926b8b0c7bd7d25a9f32aa
haholik <60041113+haholik@users.noreply.github.com>
2022-12-03 09:53:01 +0500

    day 9-10 add

--- d9-10z4.py
+++ d9-10z4.py
@@ -4,13 +4,8 @@ lizt_ip = re.split("[.]",ip)
 z=int(0)
 x=int(256)
 
-
 for i in lizt_ip:
-    if int(i) not in range(z, x):
-        print(i)
-        print("Неправильный IP-адрес")
-        break
-    elif len(lizt_ip) == 4 and re.split(ip, "."):
+    if len(lizt_ip) == 4 and re.split(ip, "."):
         if int(lizt_ip[0]) in range(1, 223):
             print("unicast")
             break
@@ -20,5 +15,11 @@ for i in lizt_ip:
         elif ip == "255.255.255.255":
             print("unassigned")
             break
+        elif int(i) not in range(z, x):
+            print(i)
+            print("Неправильный IP-адрес")
+            ip = input("Введите ip заново: ")
+            lizt_ip = re.split("[.]", ip)
         else:
             print("unused")
+            break
