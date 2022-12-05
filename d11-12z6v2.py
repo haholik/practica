9323cfcdff8d698f30c7d18031e191b2efb67b7f
haholik <game31pro@mail.ru>
2022-12-05 23:14:22 +0600

    remove 6

--- d11-12z6v2.py
+++ d11-12z6v2.py
@@ -1,13 +1,14 @@
 from tabulate import tabulate
+
 file = open("CAM_table.txt", mode="r+", encoding="utf-8")
 lizt = []
 keyer = {}
 
-for i, line in enumerate(file):
+for i, line in enumerate(file):  # присвоение каждой строке числа
     if "DYNAMIC" in line:
-        lizt = str(line).split()
-        lizt.insert(0, int(lizt[0]))
-        lizt.remove(lizt[1])
-        keyer[i] = lizt
-        del keyer[i][2]
-print(tabulate(sorted(list(keyer.values()))))
+        lizt = str(line).split()  # разбить строки на слова в список
+        lizt.insert(0, int(lizt[0]))  # в начало строки добавляется копия 1 слова из листа, но в виде числа
+        lizt.remove(lizt[1])  # удаление числа в формате строки
+        keyer[i] = lizt  # добавление в словарь строчку
+        del keyer[i][2]  # удалить строку "DYNAMIC" из листа
+print(tabulate(sorted(list(keyer.values()))))  # сортировка по значению и вывод числа в виде таблицы
