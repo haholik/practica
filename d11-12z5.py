0dcbc2c93657dea66457b6d727e242704e288289
haholik <game31pro@mail.ru>
2022-12-05 18:43:05 +0600

    add file

--- d11-12z5.py
+++ d11-12z5.py
@@ -1,5 +1,17 @@
-file = open("CAM_table.txt", mode="r+", encoding="utf-8")
+from tabulate import tabulate
 
+keyer = {}
+file = open("CAM_table.txt", mode="r+", encoding="utf-8")
 for i, line in enumerate(file):
-    if i > 3:
-        print(line,end="")
+    if "DYNAMIC" in line:
+        keyer[i] = str(line).split()
+        del keyer[i][2]
+        table = [keyer[i]]
+        print(tabulate(table, tablefmt="github"))
+
+
+
+
+
+
+
