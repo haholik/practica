7c72fb4b94e96318a6bf775a21c7822d6ac959f2
haholik <game31pro@mail.ru>
2022-12-08 16:37:54 +0600

    Z V

--- d15/z2.py
+++ d15/z2.py
@@ -10,10 +10,25 @@ def create_network_map(filenames):
     for file in filenames:
         with open(file, mode="r+") as f:
             file = f.read()
-            file = file.split()
             q = {}
-
-    return q
+            file = file.split('\n')
+            for line in file:
+                main = []
+                second = []
+                if line.find('SW') != -1:
+                    main_machine = "SW"
+                elif line.find('Eth') != -1:
+                    second_machine, main_eth, main_inter, *other, second_eth, second_inter = line.split()
+                    main.append(main_machine)
+                    second.append(second_machine)
+                    main_interface = main_eth + main_inter
+                    second_interface = second_eth + second_inter
+                    main.append(main_interface)
+                    second.append(second_interface)
+                    main_tuple = tuple(main)
+                    second_tuple = tuple(second)
+                    q[main_tuple] = second_tuple
+        return q
 
 
 if __name__ == "__main__":
